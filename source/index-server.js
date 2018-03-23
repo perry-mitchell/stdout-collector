9ba49223eb9ff543c9148be0923b58d2a5bd5188
Perry Mitchell <perry@perrymitchell.net>
2018-03-23 12:17:03 +0200

    Format files

--- source/index-server.js
+++ source/index-server.js
@@ -9,29 +9,31 @@ app.use(bodyParser.json());
 const clientIDs = [];
 
 function getClientIndex(id) {
-	const index = clientIDs.indexOf(id);
-	if (index === -1) {
-		return clientIDs.push(id) - 1;
-	}
-	return index;
+    const index = clientIDs.indexOf(id);
+    if (index === -1) {
+        return clientIDs.push(id) - 1;
+    }
+    return index;
 }
 
 function isNewClient(id) {
-	return clientIDs.indexOf(id) === -1;
+    return clientIDs.indexOf(id) === -1;
 }
 
 app.post("/", (req, res) => {
-	const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-	const { line, id } = req.body;
-	const isNew = isNewClient(id);
-	const clientIndex = getClientIndex(id);
-	if (isNew) {
-		console.log(`√ Connected: ${ip} (${clientIndex})`);
-	}
-	console.log(`${clientIndex} » ${line}`);
-	res.send(JSON.stringify({
-		status: "ok"
-	}));
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    const { line, id } = req.body;
+    const isNew = isNewClient(id);
+    const clientIndex = getClientIndex(id);
+    if (isNew) {
+        console.log(`√ Connected: ${ip} (${clientIndex})`);
+    }
+    console.log(`${clientIndex} » ${line}`);
+    res.send(
+        JSON.stringify({
+            status: "ok"
+        })
+    );
 });
 
 app.listen(8888, () => {
